e18a39fdb334913e0d3d3e11811447bc2802237b
Thien Nguyen <nvktvan@gmail.com>
2024-10-31 21:38:39 +0000

    🐧 from unlv w luv

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
 }
 
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
