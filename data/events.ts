659dab362a7c0c2ad1e56b7a25a89387483648a6
Daniel Mamuza <94477022+MamuzaD@users.noreply.github.com>
2025-04-15 20:23:28 -0700

    update event time

--- data/events.ts
+++ data/events.ts
@@ -17,7 +17,7 @@ export const Events: Event[] = [
     title: 'Code-a-thon with GWC',
     date: new Date('2025-04-30T00:00:00'),
     startTime: new Date('2025-04-30T17:30:00'),
-    endTime: new Date('2025-04-30T00:00:00'),
+    endTime: new Date('2025-04-30T20:00:00'),
     description: `Join us for a HackerRank style Code-a-thon in collaboration with GWC! Get ready to test your coding skills!`,
     location: `AEB Flexatorium`,
     type: 'Competition',
